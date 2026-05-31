b0c70086ad6d8f494f42acc656ec9b997f61a8c7
Harel-study <Harel5004@gmail.com>
2026-05-31 14:42:45 +0300

    basic server

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files (if any)
+app.use(express.static(path.join(__dirname)));
+
+// Route for the homepage
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'homepage.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
