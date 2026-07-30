5d8e03492427b9f8c44d616ae5ee33a8f3931b46
Oz Nakash <oznakash12@gmail.com>
2026-07-30 13:17:54 +0300

    fix: update MongoDB URI in .env.example and log connection URI in db.tsTED

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectDB = async (): Promise<void> => {
   try {
+    console.log(process.env.MONGODB_URI);
     const mongoURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/scoutify';
     const conn = await mongoose.connect(mongoURI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
