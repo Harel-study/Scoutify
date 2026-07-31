746ce32b8878d7f2363439708bf1e255b0462747
daniel <daniel.reiwork@gmail.com>
2026-07-31 12:35:41 +0300

    refactor: use logger insted of console.log in db configuration

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,13 +1,14 @@
 import mongoose from 'mongoose';
+import logger from './logger.js';
 
 export const connectDB = async (): Promise<void> => {
   try {
-    console.log(process.env.MONGODB_URI);
+    logger.info(process.env.MONGODB_URI || 'undefined');
     const mongoURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/scoutify';
     const conn = await mongoose.connect(mongoURI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    logger.info(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error: any) {
-    console.error(`Error connecting to MongoDB: ${error.message}`);
+    logger.error(`Error connecting to MongoDB: ${error.message}`);
     process.exit(1);
   }
 };
